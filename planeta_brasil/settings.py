dc18837426cb5d8486785b60fb91f1f2943bf9ae
Victor Costa <contato@victorfontes.com>
2014-05-05 22:36:50 -0300

    fixing DATABASES setting

--- planeta_brasil/settings.py
+++ planeta_brasil/settings.py
@@ -46,7 +46,7 @@ WSGI_APPLICATION = 'planeta_brasil.wsgi.application'
 #DATABASES = { 'default': { 'ENGINE': 'django.db.backends.sqlite3', 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), }}
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES =  { 'default':  dj_database_url.config() }
 
 
 # Internationalization
